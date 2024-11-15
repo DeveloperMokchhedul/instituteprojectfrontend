b5be88d0d89bbd8b070960976ea7e3b10732abd4
mokchhedul islam <mokchheduls46@gmail.com>
2024-11-15 15:40:34 +0600

    data updated

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -10,13 +10,13 @@ function Profile() {
       <div className="w-1/2 mx-auto bg-slate-500 h-screen">
         <div className="flex items-center justify-center h-full gap-20"> 
             <div>
-                <img className="w-[150px] h-[150px] rounded-full" src={currentUser.data.data.user.image} alt="" />
+                {/* <img className="w-[150px] h-[150px] rounded-full" src={currentUser.data.data.user.image} alt="" /> */}
                 
             </div>
             <div>
-                <p>Name: <span className="capitalize">{currentUser.data.data.user.name}</span></p>
+                {/* <p>Name: <span className="capitalize">{currentUser.data.data.user.name}</span></p>
                 <p>Email: <span className="capitalize">{currentUser.data.data.user.email}</span></p>
-                <p>Phone: <span className="capitalize">{currentUser.data.data.user.phone}</span></p>
+                <p>Phone: <span className="capitalize">{currentUser.data.data.user.phone}</span></p> */}
             </div>
         </div>
       </div>
