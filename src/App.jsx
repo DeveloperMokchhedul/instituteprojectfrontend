0a1a473579646cc9bce1dc5be7d61ee0107e942b
DeveloperMokchhedul <mokchheduls46@gmail.com>
2024-11-20 12:47:04 +0600

    data update

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,27 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Navbar from "./components/navbar/Navbar";
+import NotFound from "./pages/NotFound";
+import Registration from "./pages/Registration";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Signin from "./pages/Signin";
+import Profile from "./pages/Profile";
+import Dashboard from "./pages/Dashboard";
+import Books from "./pages/Books";
+import SingleBook from "./pages/SingleBook";
+import Cart from "./pages/Cart";
+import PrivateRoute from "./components/PrivateRoute";
+import Order from "./pages/Order";
+import OrderConferm from "./pages/OrderConferm";
+import DashBoardLayout from "./components/dashboard/DashBoardLayout";
+import AddBook from "./components/dashboard/sellerdashboard/AddBook";
+import UserDashboard from "./components/dashboard/userdashboard/UserDashboard";
+import ShowProduct from "./components/dashboard/sellerdashboard/ShowProduct";
+import OrderProduct from "./components/dashboard/sellerdashboard/OrderProduct";
+import Footer from "./components/Footer";
+import { useSelector } from "react-redux";
 function App() {
   const { currentUser } = useSelector((state) => state.user);
   const role = currentUser?.data?.data?.user?.role || "guest";
