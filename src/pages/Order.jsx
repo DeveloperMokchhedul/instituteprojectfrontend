4f613e41f19ececb34c5228dc1bd87592ffcdeed
DeveloperMokchhedul <mokchheduls46@gmail.com>
2024-11-20 12:37:58 +0600

    data update

--- src/pages/Order.jsx
+++ src/pages/Order.jsx
@@ -1,74 +1,85 @@
-import React, { useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { Link, useNavigate } from "react-router-dom";
-import axios from "axios";
-import { toast } from "react-toastify";
-import UserDetails from "../components/Checkout/UserDetails";
-import OrderSamary from "../components/Checkout/OrderSamary";
-import { removeAllCartItem } from "../redux/slice/cartSlice";
+// import React, { useState } from "react";
+// import { useDispatch, useSelector } from "react-redux";
+// import { Link, useNavigate } from "react-router-dom";
+// import axios from "axios";
+// import { toast } from "react-toastify";
+// import UserDetails from "../components/Checkout/UserDetails";
+// import OrderSamary from "../components/Checkout/OrderSamary";
+// import { removeAllCartItem } from "../redux/slice/cartSlice";
+
+// function Order() {
+//   const { totalPrice, cartItem, totalQuantity } = useSelector((state) => state.cart);
+//   const [userData, setUserData] = useState({});
+//   const navigate = useNavigate();
+//   const dispatch = useDispatch()
+
+//   const handleChange = (e) => {
+//     const { name, value } = e.target;
+//     setUserData((prevData) => ({
+//       ...prevData,
+//       [name]: value,
+//     }));
+//   };
+
+//   const orderData = {
+//     firstname: userData.firstname,
+//     lastname: userData.lastname,
+//     district: userData.district,
+//     city: userData.city,
+//     zip: userData.zip,
+//     phone: userData.phone,
+//     bookId:cartItem.map((item)=>item._id),
+//     book: cartItem.map((item) => `${item.bookname} × ${item.quantity}`),
+//     totalprice: totalPrice,
+//   };
+
+//   const placeOrderHandle = async () => {
+//     // Validate input
+//     if (!userData.firstname || !userData.lastname || !userData.district) {
+//       return toast.error("Please fill in all required fields!");
+//     }
+
+//     try {
+//       const res = await axios.post("https://instituteprojectbackend.onrender.com/api/order/addorder", orderData, {
+//         headers: {
+//           "Content-Type": "application/json",
+//         },
+//         withCredentials: true,
+//       });
+
+//       if (res.status === 201) {
+//         toast.success("Order placed successfully!");
+//         navigate("/dashboard"); 
+//       }
+//     } catch (error) {
+//       toast.error(error.response?.data?.message || error.message);
+//     }
+
+//     dispatch(removeAllCartItem())
+
+
+//   };
+
+//   return (
+//     <div className="my-10 mx-[20px]">
+//       <h1 className="text-2xl font-bold">Enter your details to complete this order</h1>
+//       <div className="flex gap-[50px]">
+//         <UserDetails handleChange={handleChange} />
+//         <OrderSamary placeOrderHandle={placeOrderHandle} />
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Order;
+
+
+import React from 'react'
 
 function Order() {
-  const { totalPrice, cartItem, totalQuantity } = useSelector((state) => state.cart);
-  const [userData, setUserData] = useState({});
-  const navigate = useNavigate();
-  const dispatch = useDispatch()
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setUserData((prevData) => ({
-      ...prevData,
-      [name]: value,
-    }));
-  };
-
-  const orderData = {
-    firstname: userData.firstname,
-    lastname: userData.lastname,
-    district: userData.district,
-    city: userData.city,
-    zip: userData.zip,
-    phone: userData.phone,
-    bookId:cartItem.map((item)=>item._id),
-    book: cartItem.map((item) => `${item.bookname} × ${item.quantity}`),
-    totalprice: totalPrice,
-  };
-
-  const placeOrderHandle = async () => {
-    // Validate input
-    if (!userData.firstname || !userData.lastname || !userData.district) {
-      return toast.error("Please fill in all required fields!");
-    }
-
-    try {
-      const res = await axios.post("https://instituteprojectbackend.onrender.com/api/order/addorder", orderData, {
-        headers: {
-          "Content-Type": "application/json",
-        },
-        withCredentials: true,
-      });
-
-      if (res.status === 201) {
-        toast.success("Order placed successfully!");
-        navigate("/dashboard"); 
-      }
-    } catch (error) {
-      toast.error(error.response?.data?.message || error.message);
-    }
-
-    dispatch(removeAllCartItem())
-
-
-  };
-
   return (
-    <div className="my-10 mx-[20px]">
-      <h1 className="text-2xl font-bold">Enter your details to complete this order</h1>
-      <div className="flex gap-[50px]">
-        <UserDetails handleChange={handleChange} />
-        <OrderSamary placeOrderHandle={placeOrderHandle} />
-      </div>
-    </div>
-  );
+    <div>Order</div>
+  )
 }
 
-export default Order;
+export default Order
