9915b09b4e1349216e4ae1f0ba1c379ca9b99ab0
DeveloperMokchhedul <mokchheduls46@gmail.com>
2024-11-20 12:33:27 +0600

    data update

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,23 +1,33 @@
-import React from 'react'
-import { useSelector } from 'react-redux';
-import SellerDashboard from '../components/dashboard/sellerdashboard/SellerDashboard';
-import UserDashboard from '../components/dashboard/userdashboard/UserDashboard';
+// import React from 'react'
+// import { useSelector } from 'react-redux';
+// import SellerDashboard from '../components/dashboard/sellerdashboard/SellerDashboard';
+// import UserDashboard from '../components/dashboard/userdashboard/UserDashboard';
 
-function Dashboard() {
-    const {currentUser,isAuthenticated} = useSelector((state)=>state.user)
-    const role = currentUser.data.data.user.role
+// function Dashboard() {
+//     const {currentUser,isAuthenticated} = useSelector((state)=>state.user)
+//     const role = currentUser.data.data.user.role
   
     
     
     
     
-  return (
-    <>
-    {
-        isAuthenticated && role==="seller"?<SellerDashboard />:<UserDashboard />
-    }
+//   return (
+//     <>
+//     {
+//         isAuthenticated && role==="seller"?<SellerDashboard />:<UserDashboard />
+//     }
 
-    </>
+//     </>
+//   )
+// }
+
+// export default Dashboard
+
+import React from 'react'
+
+function Dashboard() {
+  return (
+    <div>Dashboard</div>
   )
 }
 
